b3897a749e40b2d19baff647141c0716142f4d17
fantinels <fantinelpaola15@gmail.com>
2022-11-30 09:48:25 -0300

    app.js com express

---
+++ app.js
@@ -0,0 +1,19 @@
+﻿const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/livros', (req, res) => {
+  res.send("Livros Procurados!");
+})
+
+app.get('/cliente', (req, res) => {
+  res.send("Clientes Procurados!");
+})
+
+app.get('/livros/id', (req, res) => {
+    res.send("Livros Procurados por ID!");
+})
+
+app.get('/clientes/id', (req, res) => {
+    res.send("Clientes Procurados por ID!");
+})
